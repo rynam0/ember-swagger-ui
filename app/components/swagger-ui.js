f7bfea2e4327dd56827078819bf2889b33749b1b
Ryan Connolly <rynam0@gmail.com>
2019-03-18 12:40:57 -0400

    Publishing to github pages

--- app/components/swagger-ui.js
+++
@@ -1,1 +0,0 @@
-export { default } from 'ember-swagger-ui/components/swagger-ui';
--- tests/dummy/app/routes/application.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default Route.extend({
-
-  setupController(controller) {
-    controller.set('authz', { name: 'api_key', value: 'my-api-key', type: 'query'} );
-  }
-
-});

59551d935256b01b7e41228ce633637c7d72d2ad
Ryan Connolly <rynam0@gmail.com>
2019-10-08 20:08:42 -0400

    update ember-try scenarios

--- config/ember-try.js
+++ config/ember-try.js
@@ -23,10 +23,10 @@ module.exports = function() {
           }
         },
         {
-          name: 'ember-lts-3.4',
+          name: 'ember-lts-3.8',
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0'
+              'ember-source': '~3.8.0'
             }
           }
         },
